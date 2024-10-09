0c84e309ec0b1e1b90362e337bc54afc87966e7d
Pradheep P <mantissa6789@gmail.com>
2024-10-10 02:29:24 +0530

    admin routes also added, should check now

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 const jwt_secret = "Secr3T";
+const PORT = 3000;
 
 mongoose
   .connect('mongodb+srv://mantissa6789:Mantis%402510@cluster0.9ramotn.mongodb.net/growthx_task')
@@ -140,4 +141,100 @@ app.get("/admins", authMiddleware(['user']), async(req, res) => {
     }
 });
 
+//admin routes
 
+app.post("/adminRegister", async(req, res) => {
+    const {name, email, password, confirmPassword} = req.body;
+
+    if(password !== confirmPassword){
+        return res.status(400).json({message : "Passwords do not match"});
+    }
+
+    try {
+        const existingAdmin = await Admin.findOne({email});
+        if(existingAdmin){
+            return res.status(400).json({message : "Admin already exists"});
+        }
+
+        const adminId = uuidv4();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdmin = new Admin({name, adminEmail:email, adminPassword:hashedPassword, role:"admin"});
+        await newAdmin.save();
+
+        const token = jwt.sign({adminId, email, role : "admin"}, jwt_secret, {expiresIn : '1h'});
+        res.status(200).json({token});
+    } catch (error) {
+        res.status(500).json({message : "Server error"});
+    }
+});
+
+app.post("/adminLogin", async(req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const admin = await Admin.findOne({email});
+        const adminId = admin.adminId;
+        if(!admin){
+            return res.status(400).json({message : "Email not found"});
+        }
+
+        const validPassword = await bcrypt.compare(password, admin.adminPassword);
+        if(!validPassword){
+            return res.status(400).json({message : "Passowrd is incorrect"});
+        }
+
+        const token = jwt.sign({adminId, email, role :"admin"}. jwt_secret, {expiresIn : '1h'});
+        res.status(200).json({token});
+    } catch (error) {
+        return res.status(500).json({message : "Server error"});
+    }
+});
+
+app.get("/assignments", authMiddleware(['admin']), async(req, res) => {
+    try {
+        const admin = await Admin.findOne(req.user.adminId).populate('assignedAssignments', 'id');
+        const assignments = admin.assignedAssignments.map(assignment => ({
+            id : assignment.id,
+            task : assignment.task
+        }));
+        res.json({assignments});
+    } catch (error) {
+        return res.status(500).json({messsage : "Server error"});
+    }
+});
+
+app.post("/assignments/:id/accept", authMiddleware(['admin']), async(req, res) => {
+    try {
+        const assignmentId = req.params.id;
+        const assignment = await Assignment.findOne({assignmentId});
+        if(!assignment){
+            return res.status(400).json({message : "Assignment not found"});
+        }
+        const admin = await Admin.findOne(req.user.adminId);
+        admin.acceptedAssignments.push(assignment);
+        await admin.save();
+        res.status(200).json({message : "Admin accepted the assignment"});
+    } catch (error) {
+        return res.status(500).json({message : "Server error"});
+    }
+});
+
+app.post("/assignments/:id/reject", authMiddleware(['admin']), async(req, res) => {
+    try {
+        const assignmentId = req.params.id;
+        const assignment = await Assignment.findOne({assignmentId});
+        if(!assignment){
+            return res.status(400).json({message : "Assignment not found"});
+        }
+        const admin = await Admin.findOne(req.user.adminId);
+        admin.rejectedAssignments.push(assignment);
+        await admin.save();
+        res.status(200).json({message : "Admin rejected the assignment"});
+    } catch (error) {
+        return res.status(500).json({message : "Server error"});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
